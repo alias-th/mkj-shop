9ac7fcd8de420fd8b93278374324f88eb8b6d3f4
Monton Onnom <monton.onnom@gmail.com>
2023-02-03 09:55:41 +0700

    fix bug

--- components/login/index.tsx
+++ components/login/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import LoginForm from "./loginForm";
+import LoginForm from "./LoginForm";
 import SignUpForm from "./SignUpForm";
 import styles from "./styles.module.css";
 interface Props {}
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,13 +1,13 @@
 import { NextPage } from "next";
 import { Box } from "@mui/material";
 import AdminAppBar from "@/components/admin/AdminAppBar";
-import { useState } from "react";
+import { FC, useState } from "react";
 import AdminDrawer from "@/components/admin/AdminDrawer";
 import AdminMainContent from "@/components/admin/AdminMainContent";
 
 interface Props {}
 
-const index: NextPage<Props> = () => {
+const AdminPage: FC<Props> = (): JSX.Element => {
   const [open, setOpen] = useState(true);
 
   const handleDrawerOpen = () => {
@@ -27,4 +27,4 @@ const index: NextPage<Props> = () => {
   );
 };
 
-export default index;
+export default AdminPage;
